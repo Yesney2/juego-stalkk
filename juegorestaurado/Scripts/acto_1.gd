cad6a6134bee7da849ff373e0e54fc73f64ee914
Vianney <jessicavianney2003@gmail.com>
2025-10-17 14:13:42 -0600

    puse ecena de final

--- juegorestaurado/Scripts/acto_1.gd
+++ juegorestaurado/Scripts/acto_1.gd
@@ -5,8 +5,7 @@ extends Node2D
 func _ready() -> void:
 	var dialog_lines := [
 		"¡Soy el Agente P.",
-		"Aquí empieza nuestra historia...",
-		"Pulsa clic o Enter para continuar."
+
 	]
 	dialog_container.start_dialog(dialog_lines)
 	dialog_container.dialog_finished.connect(Callable(self, "_on_dialog_finished"))
